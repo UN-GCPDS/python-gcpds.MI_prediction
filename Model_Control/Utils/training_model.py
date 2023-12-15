7a9fbfa51bee6213300faba2904cd37cebb3e7fa
Sebastian Mendez <mendezsebas8@gmail.com>
2023-12-15 07:22:18 -0500

    "Cambios"

--- Model_Control/Utils/training_model.py
+++ Model_Control/Utils/training_model.py
@@ -210,7 +210,7 @@ def redirectToTrain(Model,callbacks,X_train,Y_train,x_val,y_val,validation_mode,
                     NO ESTA ADECUADO PARA AUTOENCODERS
                     ----------------------------------
                     """
-
+                   
                     preds = []
                     y_true = []
                     acc = []
@@ -218,32 +218,61 @@ def redirectToTrain(Model,callbacks,X_train,Y_train,x_val,y_val,validation_mode,
 
                     skf = StratifiedKFold(n_splits=4)
                     print("data genial: ")
-                    for train_index, test_index in skf.split(X_train, Y_train):
-                        print("data: ",train_index, test_index)
-                        X_train_, X_test_ = X_train[train_index], X_train[test_index]
-                        y_train_, y_test_ = Y_train[train_index], Y_train[test_index]
 
-                        x_tr, x_v, y_tr, y_v = train_test_split(X_train_, y_train_, test_size=0.3,random_state=seed)
+                    if(autoencoder):
+                        for train_index, test_index in skf.split(X_train, Y_train[1]):
+                            print("data: ",train_index, test_index)
+                            X_train_, X_test_ = X_train[train_index], X_train[1][test_index]
+                            y_train_, y_test_ = Y_train[train_index], Y_train[1][test_index]
 
-                        callbacks_names = [callbacks['checkpoint_train'+str(c+1)],callbacks['early_stopping_train'+str(c+1)]]
+                            x_tr, x_v, y_tr, y_v = train_test_split(X_train_, y_train_, test_size=0.3,random_state=seed)
 
-                        history= Model.fit(x_tr,tf.keras.utils.to_categorical(y_tr,num_classes=2),validation_data=(x_v, tf.keras.utils.to_categorical(y_v,num_classes=2)),batch_size=batchSize,epochs=epochs,verbose=verbose,callbacks=callbacks_names)
-                        History.append(history)
-                        
-                        Model.load_weights(callbacks['checkpoint_train'+str(c+1)].filepath)
-                        pred = Model.predict(X_test_)
-                        preds.append(pred)
-                        y_preds = preds[c].argmax(axis = -1)
-                        y_true.append(y_test_)
-                        acc.append(np.mean(y_preds == y_test_))
-                        print("Fold %d Classification accuracy: %f " % (c+1,acc[c]))
-                        c += 1
+                            callbacks_names = [callbacks['checkpoint_train'+str(c+1)],callbacks['early_stopping_train'+str(c+1)]]
 
-                    preds = np.concatenate(preds,axis=0)
-                    y_true = np.concatenate(y_true,axis=0)
-                    acc = get_accuracy(preds,tf.keras.utils.to_categorical(y_true,num_classes=2),decimals=2)
+                            history= Model.fit(x_tr,[x_tr,tf.keras.utils.to_categorical(y_tr,num_classes=2)],validation_data=(x_v, [x_v,tf.keras.utils.to_categorical(y_v,num_classes=2)]),batch_size=batchSize,epochs=epochs,verbose=verbose,callbacks=callbacks_names)
+                            History.append(history)
+                            
+                            Model.load_weights(callbacks['checkpoint_train'+str(c+1)].filepath)
+                            pred = Model.predict(X_test_)
+                            preds.append(pred[1])
+                            y_preds = preds[c].argmax(axis = -1)
+                            y_true.append(y_test_)
+                            acc.append(np.mean(y_preds == y_test_))
+                            print("Fold %d Classification accuracy: %f " % (c+1,acc[c]))
+                            c += 1
 
-                    return Model,History,acc
+                        preds = np.concatenate(preds,axis=0)
+                        y_true = np.concatenate(y_true,axis=0)
+                        acc = get_accuracy(preds,tf.keras.utils.to_categorical(y_true,num_classes=2),decimals=2)
+
+                        return Model,History,acc
+                    else:
+                        for train_index, test_index in skf.split(X_train, Y_train):
+                            print("data: ",train_index, test_index)
+                            X_train_, X_test_ = X_train[train_index], X_train[test_index]
+                            y_train_, y_test_ = Y_train[train_index], Y_train[test_index]
+
+                            x_tr, x_v, y_tr, y_v = train_test_split(X_train_, y_train_, test_size=0.3,random_state=seed)
+
+                            callbacks_names = [callbacks['checkpoint_train'+str(c+1)],callbacks['early_stopping_train'+str(c+1)]]
+
+                            history= Model.fit(x_tr,tf.keras.utils.to_categorical(y_tr,num_classes=2),validation_data=(x_v, tf.keras.utils.to_categorical(y_v,num_classes=2)),batch_size=batchSize,epochs=epochs,verbose=verbose,callbacks=callbacks_names)
+                            History.append(history)
+                            
+                            Model.load_weights(callbacks['checkpoint_train'+str(c+1)].filepath)
+                            pred = Model.predict(X_test_)
+                            preds.append(pred)
+                            y_preds = preds[c].argmax(axis = -1)
+                            y_true.append(y_test_)
+                            acc.append(np.mean(y_preds == y_test_))
+                            print("Fold %d Classification accuracy: %f " % (c+1,acc[c]))
+                            c += 1
+
+                        preds = np.concatenate(preds,axis=0)
+                        y_true = np.concatenate(y_true,axis=0)
+                        acc = get_accuracy(preds,tf.keras.utils.to_categorical(y_true,num_classes=2),decimals=2)
+
+                        return Model,History,acc
 
 
 

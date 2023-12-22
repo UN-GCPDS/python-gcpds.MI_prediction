5e3c8eb0266f662cc132a236af163fc4a19dcaa7
Sebastian Mendez <mendezsebas8@gmail.com>
2023-12-22 14:17:44 -0500

    "CAMBIOS"

--- Model_Control/Utils/training_model.py
+++ Model_Control/Utils/training_model.py
@@ -284,7 +284,7 @@ def redirectToTrain(Model,callbacks,X_train,Y_train,x_val,y_val,validation_mode,
 
                             callbacks_names = [callbacks['checkpoint_train'+str(c+1)],callbacks['early_stopping_train'+str(c+1)]]
 
-                            history= Model.fit(x_tr,tf.keras.utils.to_categorical(y_tr,num_classes=len(np.unique(Y_train))),validation_data=(x_v, tf.keras.utils.to_categorical(y_v,num_classes=len(np.unique(Y_train)))),batch_size=batchSize,epochs=epochs,verbose=verbose,callbacks=callbacks_names)
+                            history= Model.fit(x_tr,tf.keras.utils.to_categorical(y_tr,num_classes=2),validation_data=(x_v, tf.keras.utils.to_categorical(y_v,num_classes=2)),batch_size=batchSize,epochs=epochs,verbose=verbose,callbacks=callbacks_names)
                             History.append(history)
                             
                             Model.load_weights(callbacks['checkpoint_train'+str(c+1)].filepath)
@@ -298,7 +298,7 @@ def redirectToTrain(Model,callbacks,X_train,Y_train,x_val,y_val,validation_mode,
 
                         preds = np.concatenate(preds,axis=0)
                         y_true = np.concatenate(y_true,axis=0)
-                        acc = get_accuracy(preds,tf.keras.utils.to_categorical(y_true,num_classes=len(np.unique(Y_train))),decimals=2)
+                        acc = get_accuracy(preds,tf.keras.utils.to_categorical(y_true,num_classes=2),decimals=2)
 
                         return Model,History,acc
 
